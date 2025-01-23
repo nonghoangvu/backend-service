054c2ce9ab6499d7ed241f1d62faf615618052a9
JavaTech <nonghoangvu2004@gmail.com>
2025-01-23 21:24:54 +0700

    init project base

---
+++ src/main/java/vn/vunh/controller/HelloController.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Nong Hoang Vu || JavaTech
+ * Facebook:https://facebook.com/NongHoangVu04
+ * Github: https://github.com/JavaTech04
+ * Youtube: https://www.youtube.com/@javatech04/?sub_confirmation=1
+ */
+package vn.vunh.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Tag(name = "Hello Controller")
+public class HelloController {
+
+    @Operation(summary = "Test API", description = "Mo tả chi tiết về API")
+    @GetMapping("/hello")
+    public String greeting(@RequestParam String name) {
+        return "Hello, " + name;
+    }
+}
+
